20b008d3ef9b8244ec4c19830f2c15283c277cbc
natginer <86863193+natginer@users.noreply.github.com>
2022-02-17 11:47:26 -0300

    Add files via upload

---
+++ counter/index.js
@@ -0,0 +1,16 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+let count = 0
+
+ function incLaps(){
+   count += 1
+   countEl.innerText = count
+ }
+
+ function save(){
+   let countStr = count + " - "
+   saveEl.innerHTML += countStr
+   countEl.innerHTML = 0;
+   count = 0;
+ }
